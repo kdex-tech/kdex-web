a55c284bb17e82fb8665666653e1b3fb6cd299b4
Raymond Augé <rayauge@doublebite.com>
2025-09-30 22:33:27 -0400

    kubebuilder create api --version v1alpha1 --kind MicroFrontEndRenderPage --resource=false --controller=true

--- cmd/main.go
+++ cmd/main.go
@@ -172,6 +172,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "MicroFrontEndHost")
 		os.Exit(1)
 	}
+	if err := (&controller.MicroFrontEndRenderPageReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "MicroFrontEndRenderPage")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
