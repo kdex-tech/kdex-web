3f2073f9f824047e906068d74b24835e48acb242
Raymond Augé <rayauge@doublebite.com>
2025-10-02 23:39:12 -0400

    add wildcard and longest match to domain matching logic

--- internal/web/middleware/host.go
+++ internal/web/middleware/host.go
@@ -5,14 +5,14 @@ import (
 	"net/http"
 	"strings"
 
-	"kdex.dev/web/internal/store"
+	store_ "kdex.dev/web/internal/store"
 )
 
 type contextKey string
 
 const HostKey contextKey = "host"
 
-func WithHost(store *store.HostStore) func(http.Handler) http.Handler {
+func WithHost(store *store_.HostStore) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			hostHeader := r.Host
@@ -23,15 +23,33 @@ func WithHost(store *store.HostStore) func(http.Handler) http.Handler {
 
 			hosts := store.List()
 
-			for _, trackedHost := range hosts {
-				host := trackedHost
+			var bestMatchHost *store_.TrackedHost
+			var bestMatchLength = -1
+
+			for _, host := range hosts {
 				for _, domain := range host.Host.Spec.Domains {
 					if domain == hostHeader {
-						ctx := context.WithValue(r.Context(), HostKey, &host)
+						ctx := context.WithValue(r.Context(), HostKey, host)
 						next.ServeHTTP(w, r.WithContext(ctx))
 						return
 					}
+
+					if strings.HasPrefix(domain, "*.") {
+						suffix := domain[1:]
+						if strings.HasSuffix(hostHeader, suffix) && len(hostHeader) > len(suffix) {
+							if len(domain) > bestMatchLength {
+								bestMatchLength = len(domain)
+								bestMatchHost = host
+							}
+						}
+					}
 				}
+			}
+
+			if bestMatchHost != nil {
+				ctx := context.WithValue(r.Context(), HostKey, bestMatchHost)
+				next.ServeHTTP(w, r.WithContext(ctx))
+				return
 			}
 
 			next.ServeHTTP(w, r)
