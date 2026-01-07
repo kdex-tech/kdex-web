775811bd594f77e7254553f4f88b3ab1edf6d0c3
Raymond Augé <rayauge@doublebite.com>
2026-01-07 13:19:41 -0500

    use correct finalizer name
    Signed-off-by: Raymond Augé <rayauge@doublebite.com>

--- internal/controller/types.go
+++ internal/controller/types.go
@@ -4,6 +4,6 @@ const (
 	BACKEND                = "backend"
 	HOST_INDEX_KEY         = "spec.hostRef.name"
 	OCI_IMAGE              = "oci-image"
-	PAGE_BINDING_FINALIZER = "kdex.dev/kdex-nexus-page-binding-finalizer"
+	PAGE_BINDING_FINALIZER = "kdex.dev/kdex-web-page-binding-finalizer"
 	TRANSLATION_FINALIZER  = "kdex.dev/kdex-web-translation-finalizer"
 )
